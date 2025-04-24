a55d874c993c787dd28861b5f3a3220a5f5d6ad0
esromneb <bjmorse@gmail.com>
2025-04-24 14:23:33 -0700

    slow here

--- src/MS5837.cpp
+++ src/MS5837.cpp
@@ -32,8 +32,8 @@ bool MS5837::init(TwoWire &wirePort) {
 	_i2cPort = &wirePort; //Grab which port the user wants us to use
 
 	// Reset the MS5837, per datasheet
-	_i2cPort->beginTransmission(MS5837_ADDR);
 	_i2cPort->setClock(10000);
+	_i2cPort->beginTransmission(MS5837_ADDR);
 	_i2cPort->write(MS5837_RESET);
 	_i2cPort->endTransmission();
 
